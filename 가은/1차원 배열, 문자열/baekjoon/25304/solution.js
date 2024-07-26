556f2fc3eed0c15de21e9f09740339bf3feb3c27
hmyo2853 <hmyo2853@gmail.com>
2024-07-27 00:02:17 +0900

    25304 업데이트

--- 가은/1차원 배열, 문자열/baekjoon/25304/solution.js
+++ 가은/1차원 배열, 문자열/baekjoon/25304/solution.js
@@ -2,6 +2,19 @@
 // 영수증
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = fs.readFileSync(filePath).toString();
+const input = fs.readFileSync(filePath).toString().split("\n");
 
-console.log(input);
+const result = Number(input[0]);
+const count = Number(input[1]);
+
+let sum = 0;
+for (let i = 2; i < count + 2; i++) {
+  const priceInfo = input[i].split(" ");
+  sum += priceInfo[0] * priceInfo[1];
+}
+
+if (sum === result) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
