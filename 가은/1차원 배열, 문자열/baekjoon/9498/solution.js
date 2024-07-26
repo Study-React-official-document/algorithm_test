3778cc419d50e2e31027293496add24d6ece2268
hmyo2853 <hmyo2853@gmail.com>
2024-07-27 00:00:14 +0900

    9498 업데이트

--- 가은/1차원 배열, 문자열/baekjoon/9498/solution.js
+++ 가은/1차원 배열, 문자열/baekjoon/9498/solution.js
@@ -2,6 +2,17 @@
 // 시험성적
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = fs.readFileSync(filePath).toString();
+const input = fs.readFileSync(filePath).toString().split(" ");
+const num = parseInt(input);
 
-console.log(input);
+if (num >= 90) {
+  console.log("A");
+} else if (num >= 80) {
+  console.log("B");
+} else if (num >= 70) {
+  console.log("C");
+} else if (num >= 60) {
+  console.log("D");
+} else {
+  console.log("F");
+}
