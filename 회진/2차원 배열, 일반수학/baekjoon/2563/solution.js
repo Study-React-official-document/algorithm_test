bd8e162b9b327377a5fc8349f0c9624e25c25e84
hjyang369 <125977702+hjyang369@users.noreply.github.com>
2024-08-05 00:13:56 +0900

    yhj-3주차 숙제

--- 회진/2차원 배열, 일반수학/baekjoon/2563/solution.js
+++ 회진/2차원 배열, 일반수학/baekjoon/2563/solution.js
@@ -1,5 +1,57 @@
 // https://www.acmicpc.net/problem/2563
-//
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = fs.readFileSync(filePath).toString().split("\n");
+// 색종이
+
+// 내 틀린 풀이
+// -> 이렇게 풀면 여러번 겹치는 경우를 확인해야해서 까다롭고, for문을 너무 많이 돌아야해서 좀 별로인 코드
+// const fs = require("fs");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+// const input = fs.readFileSync(filePath).toString().trim().split("\n");
+// const N = input[0];
+// const paper = input.slice(1).map((ele) => ele.split(" ").map(Number));
+
+// let result = 100 * N;
+
+// for (let i = 0; i < N; i++) {
+//   const [x, y] = paper[i];
+//   for (let j = i + 1; j < N; j++) {
+//     const [a, b] = paper[j];
+
+//     if (Math.abs(x - a) < 10 && Math.abs(y - b) < 10) {
+//       if (result > 100) {
+//         result -= Math.abs(x + 10 - a) * Math.abs(b + 10 - y);
+//       }
+//     }
+//   }
+// }
+
+// console.log(result);
+
+// 다른 사람 풀이
+const filePath = `linux` === process.platform ? `dev/stdin` : "input.txt";
+const lines = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(`\n`);
+
+let drawingPaper = Array.from(Array(100), () => Array(100).fill(0)); // 100*100 배열 생성
+const confetti = lines.slice(1).map((line) => line.split(` `).map(Number)); // 예시 배열
+let size = 0;
+
+if (Number(lines[0]) !== confetti.length) return;
+
+confetti.forEach((line) => {
+  const [x, y] = line;
+
+  for (let i = x; i < x + 10; i++) {
+    // 색종이의 가로 길이만큼
+    for (let j = y; j < y + 10; j++) {
+      // 색종이의 세로 길이만큼 순회
+      if (1 === drawingPaper[i][j]) continue; // 만약 값이 1이라면 사이즈 1 올려줌
+      drawingPaper[i][j] = 1;
+      size++;
+    }
+  }
+});
+
+console.log(size);
--- 회진/2차원 배열, 일반수학/baekjoon/2566/solution.js
+++ 회진/2차원 배열, 일반수학/baekjoon/2566/solution.js
@@ -1,5 +1,32 @@
-// https://www.acmicpc.net/problem/2563
-//
+// https://www.acmicpc.net/problem/2566
+// 최댓값
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = fs.readFileSync(filePath).toString().split("\n");
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const arr = input.map((ele) => ele.split(" ").map(Number));
+
+let max = arr[0][0];
+let location = [0, 0];
+
+for (let i = 0; i < 9; i++) {
+  for (let j = 0; j < 9; j++) {
+    if (max <= arr[i][j]) {
+      max = arr[i][j];
+      location[0] = i + 1;
+      location[1] = j + 1;
+    }
+  }
+}
+
+console.log(max, ...location);
+
+// const _max = [];
+// input.map(row => {
+//     _max.push(Math.max(...row));
+// })
+
+// const max = [Math.max(..._max)];
+// max.push(_max.findIndex(item => item === max[0]) + 1);
+// max.push(input[max[1] - 1].findIndex(item => item === max[0]) + 1)
+
+// console.log(`${max[0]}\n${max[1]} ${max[2]}`)
