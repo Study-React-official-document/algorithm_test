a036e772a19754ca2038e793ba1341d56684795a
hmyo2853 <hmyo2853@gmail.com>
2024-08-04 19:34:40 +0900

    2563 풀이

--- 가은/2차원 배열, 일반수학/baekjoon/2563/solution.js
+++ 가은/2차원 배열, 일반수학/baekjoon/2563/solution.js
@@ -1,5 +1,30 @@
 // https://www.acmicpc.net/problem/2563
-//
+// 색종이
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().split("\n");
+const length = Number(input[0]);
+let count = 0;
+
+let array = new Array(100).fill(null).map(() => new Array(100).fill(null));
+
+for (let i = 1; i < length + 1; i++) {
+  const [num1, num2] = input[i].split(" ").map(Number);
+  fillArray(num1, num2);
+}
+
+function fillArray(num1, num2) {
+  for (let i = num1; i < num1 + 10; i++) {
+    for (let j = num2; j < num2 + 10; j++) {
+      array[i][j] = 1;
+    }
+  }
+}
+
+array.forEach((item) => {
+  item.forEach((e) => {
+    if (e === 1) count++;
+  });
+});
+
+console.log(count);
