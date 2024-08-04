f38d2a92ad5ceab6584d876f2876b2a74eb513ae
hjyang369 <125977702+hjyang369@users.noreply.github.com>
2024-08-04 22:58:54 +0900

    3주차 준비

---
+++ 회진/2차원 배열, 일반수학/baekjoon/2566/solution.js
@@ -0,0 +1,5 @@
+// https://www.acmicpc.net/problem/2563
+//
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n");
