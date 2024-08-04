2ae8acd94df5d9dcb2e4c4a7b1727268271854fa
hmyo2853 <hmyo2853@gmail.com>
2024-08-04 19:49:35 +0900

    2566 풀이

--- 가은/2차원 배열, 일반수학/baekjoon/2566/solution.js
+++ 가은/2차원 배열, 일반수학/baekjoon/2566/solution.js
@@ -1,5 +1,22 @@
-// https://www.acmicpc.net/problem/2563
-//
+// https://www.acmicpc.net/problem/2566
+// 최댓값
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().split("\n");
+
+let maxNum = 0;
+let row = 0;
+let column = 0;
+
+for (let i = 0; i < input.length; i++) {
+  const arr = input[i].split(" ").map(Number);
+  const max = Math.max(...arr);
+  if (max > maxNum) {
+    maxNum = max;
+    row = i;
+    column = arr.indexOf(max);
+  }
+}
+
+console.log(maxNum);
+console.log(`${row + 1} ${column + 1}`);
