e81899f1109256cb3d1a280d6e27350fb3d77b1a
hjyang369 <125977702+hjyang369@users.noreply.github.com>
2024-08-19 22:32:07 +0900

    yhj-5주차 숙제2

--- 회진/재귀, 백트래킹/baekjoon/15649/solution.js
+++ 회진/재귀, 백트래킹/baekjoon/15649/solution.js
@@ -29,3 +29,33 @@ const dfs = (count) => {
 };
 
 dfs(0);
+
+// 못 풀어서 다른 사람거 보고 풀었음
+// 백트래킹 = dfs 어렵다..
+// 방문했냐 안했냐로 풀어야한다는것을 깨달음
+
+// string으로 한번에 뽑는 방법
+// const solve = (depth) => {
+//   if (depth === M) {
+//     output += save.join(" ");
+//     output += "\n";
+//     return;
+//   }
+
+//   for (let i = 1; i <= N; i++) {
+//     if (!isChecked[i]) {
+//       isChecked[i] = true;
+//       save[depth] = i;
+//       solve(depth + 1);
+//       isChecked[i] = false;
+//     }
+//   }
+// };
+
+// const isChecked = new Array(N + 1).fill(false);
+// const save = new Array(M).fill(0);
+// let output = "";
+
+// solve(0);
+
+// console.log(output);
